4efd5b195e7c859209039a210eb93e0ad293a595
GiangNXT <Giang.nxt.it@gmail.com>
2023-07-25 13:51:46 +0700

    Init project

---
+++ src/main/java/org/example/plugin/GreetingPlugin.java
@@ -0,0 +1,12 @@
+package org.example.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GreetingPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.task("hello")
+                .doLast(task -> System.out.println("Hello Gradle!"));
+    }
+}
